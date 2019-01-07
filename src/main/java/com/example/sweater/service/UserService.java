6de2bef6b589c5df2085c069db1f811a5cb1d00d
gagarina6794 <1331gagarina>
2019-01-08 00:39:38 +0200

    bean_validation, password validation

--- src/main/java/com/example/sweater/service/UserService.java
+++ src/main/java/com/example/sweater/service/UserService.java
@@ -3,9 +3,11 @@ package com.example.sweater.service;
 import com.example.sweater.domain.Role;
 import com.example.sweater.domain.User;
 import com.example.sweater.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -21,6 +23,9 @@ public class UserService implements UserDetailsService {
     private final UserRepo userRepo;
     private final MailSender mailSender;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public UserService(UserRepo userRepo, MailSender mailSender) {
         this.userRepo = userRepo;
         this.mailSender = mailSender;
@@ -28,7 +33,13 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        return userRepo.findByUsername(userName);
+       User user =userRepo.findByUsername(userName);
+
+       if(user == null){
+           throw new UsernameNotFoundException("User not found");
+       }
+
+        return user;
     }
 
     public boolean addUser(User user) {
@@ -41,6 +52,7 @@ public class UserService implements UserDetailsService {
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
         user.setActivationCode(UUID.randomUUID().toString());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         userRepo.save(user);
 
